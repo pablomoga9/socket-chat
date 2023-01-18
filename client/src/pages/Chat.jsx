e672f90099e2a5443bd29b1e3cab4d2e864a4cec
pablomoga9 <moyagarcia99@gmail.com>
2023-01-18 12:49:56 +0100

    add/logout function

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,78 +1,83 @@
-import React, {useState, useEffect} from 'react'
-import styled from 'styled-components';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { allUsersRoute } from '../utils/APIRoutes';
-import Contacts from '../components/Contacts';
-import { constants } from 'buffer';
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { io } from "socket.io-client";
+import styled from "styled-components";
+import { allUsersRoute, host } from "../utils/APIRoutes";
+import ChatContainer from "../components/ChatContainer";
+import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
 
-function Chat() {
-  const [contacts,setContacts] = useState([]);
+export default function Chat() {
   const navigate = useNavigate();
-  const [currentUser,setCurrentUser] = useState(undefined)
-  useEffect(()=>{
-    const controlUser = async()=>{
-      try{
-        if (!localStorage.getItem("chat-app-user"))
-        navigate("/login")
-        else{
-           setCurrentUser( await JSON.parse(localStorage.getItem("chat-app-user")))
-        }
-      }
-      catch(error){
-         console.log(error)
+  const socket = useRef();
+  const [contacts, setContacts] = useState([]);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  useEffect(async () => {
+    if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+      navigate("/login");
+    } else {
+      setCurrentUser(
+        await JSON.parse(
+          localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+        )
+      );
+    }
+  }, []);
+  useEffect(() => {
+    if (currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);
+    }
+  }, [currentUser]);
+
+  useEffect(async () => {
+    if (currentUser) {
+      if (currentUser.isAvatarImageSet) {
+        const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+        setContacts(data.data);
+      } else {
+        navigate("/setAvatar");
       }
     }
-    controlUser();
-   
-  },[])
-  useEffect(()=>{
-    const getUser = async()=>{
-      try{  
-        if(currentUser){
-            if(currentUser.isAvatarImageSet){
-            const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
-            setContacts(data.data);
-          }
-          else{
-            navigate("/setAvatar");
-          }
-        }
-      }
-      catch(error){
-        console.log(error)
-      }
-    }
-    getUser();
-  },[currentUser])
+  }, [currentUser]);
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
   return (
-    <Container>
-      <div className="container">
-        <Contacts contacts={contacts} currentUser={currentUser}/>
-      </div>
-    </Container>
-  )
+    <>
+      <Container>
+        <div className="container">
+          <Contacts contacts={contacts} changeChat={handleChatChange} />
+          {currentChat === undefined ? (
+            <Welcome />
+          ) : (
+            <ChatContainer currentChat={currentChat} socket={socket} />
+          )}
+        </div>
+      </Container>
+    </>
+  );
 }
 
 const Container = styled.div`
   height: 100vh;
   width: 100vw;
-  display:flex;
+  display: flex;
   flex-direction: column;
   justify-content: center;
   gap: 1rem;
   align-items: center;
   background-color: #131324;
-  .container{
+  .container {
     height: 85vh;
     width: 85vw;
     background-color: #00000076;
     display: grid;
     grid-template-columns: 25% 75%;
-    @media screen and (min-width: 720px) and (max-width: 1080px){
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
       grid-template-columns: 35% 65%;
     }
   }
 `;
-
-export default Chat
